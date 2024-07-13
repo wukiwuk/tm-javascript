5f02b47165a6a2b6d876f00d9da3e0beda223413
wukiwuk <shaquille.fazhira@gmail.com>
2024-07-13 15:04:48 +0700

    99%

--- code.js
+++ code.js
@@ -2,11 +2,36 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container")
 
 const addTask = () => {
-    if (inputBox.value === '') {
-        alert("Write a task!");
+    if(inputBox.value === '') {
+        alert("No input detected!");
     } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = "";
+    saveData()
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+const saveData = () => {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+const showTask = () => {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask()
